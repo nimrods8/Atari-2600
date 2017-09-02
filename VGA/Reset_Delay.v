9eb96a19f7ca0fda57013e5613b877bc2da7da23
nimrods8 <31564966+nimrods8@users.noreply.github.com>
2017-09-02 16:30:17 +0300

    Add files via upload

--- VGA/Reset_Delay.v
+++ VGA/Reset_Delay.v
@@ -1,1 +1,21 @@
+module	Reset_Delay(iCLK,oRESET, iRESET);
+	input		iCLK;
+	input    iRESET;
+	output reg	oRESET;
 
+	reg	[19:0]	Cont;
+
+	always@(posedge iCLK)
+	begin
+		if( ~iRESET)
+			Cont <= 0;
+		
+		if( Cont != 20'hFFFFF)
+		begin
+			Cont	<=	Cont+1;
+			oRESET	<=	1'b0;
+		end
+		else oRESET	<=	1'b1;
+	end
+
+endmodule
